dcb9b987d115f9a7a62ab39801470ab09f9207dc
ndqchi <59836089+ndqchi@users.noreply.github.com>
2020-03-12 13:10:07 +0800

    Assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle
-		, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+		, Breadcrumb, BreadcrumbItem, Label, Modal, ModalHeader,
+		ModalBody, Button, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, Errors, LocalForm} from 'react-redux-form'
 
-	function RenderComments({comments}) {
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class CommentForm extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isModalOpen: false
+		}
+
+		this.toggleModal = this.toggleModal.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	toggleModal() {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen
+		});
+	}
+
+	handleSubmit(values) {
+		this.toggleModal();
+		alert("Current State is: "+JSON.stringify(values));
+	}
+
+	render() {
+		return (
+			<div>
+				<Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+				<Modal isOpen = {this.state.isModalOpen} toggle={this.toggleModal}>
+					<ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+					<ModalBody>
+						<LocalForm onSubmit = {(values) => this.handleSubmit(values)}>
+							<Row className="form-group">
+								<Col>
+	                            <Label htmlFor="rating">Rating</Label>
+	                            <Control.select model=".rating" id="rating" className="form-control">
+	                                <option>1</option>
+	                                <option>2</option>
+	                                <option>3</option>
+	                                <option>4</option>
+	                                <option>5</option>
+	                            </Control.select>
+	                            </Col>
+							</Row>
+							<Row className="form-group">
+								<Col>
+								<Label htmlFor="yourname">Your Name</Label>
+								<Control.text model=".yourname" id="yourname" className="form-control"
+									name = "yourname"
+									placeholder="Your Name"
+									validators={{
+										maxLength: maxLength(15), minLength: minLength(3)
+									}} />
+
+								<Errors
+	                            	className="text-danger"
+	                            	model=".yourname"
+	                            	show="touched"
+	                            	messages= {{
+	                            		minLength: 'Must be greater than 2 characters',
+	                            		maxLength: 'Must be 15 characters or less'
+	                            	}}
+	                            />
+	                            </Col>
+							</Row>
+							<Row className="form-group">
+								<Col>
+								<Label htmlFor="comment">Comment</Label>
+								<Control.textarea model=".comment" id="comment" name="comment"
+									rows="6" className="form-control" />
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Col>
+								<Button color="primary" type="submit">
+								Submit
+								</Button>
+								</Col>
+							</Row>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+			</div>
+		)
+	}
+}
+
+function RenderComments({comments}) {
 		if (comments!=null) {
 			return (
 				<div>
@@ -22,14 +113,15 @@ import { Link } from 'react-router-dom';
 							);
 						})}
 					</ul>
+					<CommentForm />
 				</div>
 			)
 		} else {
 			return (<div></div>)
 		}
-	}
+}
 
-	function RenderDish({dish}) {
+function RenderDish({dish}) {
 		if (dish!=null) {
 			return (
 					<Card>
@@ -44,9 +136,9 @@ import { Link } from 'react-router-dom';
 		else {
 			return (<div></div>)
 		}
-	}
+}
 
-	const DishDetail = (props) => {
+const DishDetail = (props) => {
 		const dish=props.dish;
 		if (dish==null) {
 			return (<div></div>)
@@ -74,7 +166,7 @@ import { Link } from 'react-router-dom';
                 </div>
                 </div>
             );
-	}
+}
 
 
 export default DishDetail;
