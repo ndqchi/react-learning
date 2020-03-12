65a0e9f7af689bf242fcc90cfe4d67e01837ccaa
ndqchi <59836089+ndqchi@users.noreply.github.com>
2020-03-12 20:43:01 +0800

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle
 		ModalBody, Button, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, Errors, LocalForm} from 'react-redux-form'
+import { Loading } from './LoadingComponent';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -139,35 +140,50 @@ function RenderDish({dish}) {
 }
 
 const DishDetail = (props) => {
-		const dish=props.dish;
-		if (dish==null) {
-			return (<div></div>)
-		}
+	if (props.isLoading) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	}
+	else if (props.errMess) {
+		return (
+			<div className="container">
+				<div className="row">
+					<h4>{props.errMess}</h4>
+				</div>
+			</div>
+		);
+	}
+	else if (props.dish!=null) {
 		return (
                 <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>                
-                </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments}
-                        	addComment={props.addComment}
-                        	dishId={props.dish.id} />
-                    </div>
-                </div>
+                	<div className="row">
+                    	<Breadcrumb>
+	                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+	                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+	                    </Breadcrumb>
+	                    <div className="col-12">
+	                        <h3>{props.dish.name}</h3>
+	                        <hr />
+	                    </div>                
+	                </div>
+	                <div className="row">
+	                    <div className="col-12 col-md-5 m-1">
+	                        <RenderDish dish={props.dish} />
+	                    </div>
+	                    <div className="col-12 col-md-5 m-1">
+	                        <RenderComments comments={props.comments}
+	                        	addComment={props.addComment}
+	                        	dishId={props.dish.id} />
+	                    </div>
+	                </div>
                 </div>
             );
+    }
 }
 
 
